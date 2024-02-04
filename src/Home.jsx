1e46efda98eecdfcbd0e8d6f351d02ebe0096a7d
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-02-04 16:58:56 +0100

    Création du component de la page d'accueil

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+import HomeBanner from "./HomeBanner";
+
+function Home() {
+  return (
+    <div>
+      <HomeBanner />
+    </div>
+  );
+}
+
+export default Home;

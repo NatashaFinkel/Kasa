088cc623296ee3b6617d24b8f79471d833620abb
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-02-04 17:57:20 +0100

    Création du composant de la NavBar et ajout dans le Router

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
 const AppRouter = () => {
   return (
     <Router>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />

21f2f3575c190d9025dca298da839ba1693da586
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-02-04 16:53:57 +0100

    Création du component de la page a propos

---
+++ src/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+import AboutBanner from "./AboutBanner";
+
+function About() {
+  return (
+    <div>
+      <AboutBanner />
+    </div>
+  );
+}
+
+export default About;

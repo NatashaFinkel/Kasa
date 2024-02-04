e6ea61a9537c8cd5f0a69c0a7525529059115cc3
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-02-04 16:40:37 +0100

    Création du component de la bannière de la page accueil

---
+++ src/HomeBanner.jsx
@@ -0,0 +1,12 @@
+import homeBannerImg from "./homeBanner.png";
+import "./index.css";
+
+function HomeBanner() {
+  return (
+    <div>
+      <img src={homeBannerImg} alt="bannière de la page d'accueil"></img>;
+    </div>
+  );
+}
+
+export default HomeBanner;

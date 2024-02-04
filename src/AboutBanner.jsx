b637e9d7f778c2f0cf60201fb05bb37f87022eeb
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-02-04 16:39:37 +0100

    Création du component de la bannière de la page A propos

---
+++ src/AboutBanner.jsx
@@ -0,0 +1,8 @@
+import aboutBannerImg from "./aboutBanner.png";
+import "./index.css";
+
+function AboutBanner() {
+  return <img src={aboutBannerImg} alt="bannière de la page À propos"></img>;
+}
+
+export default AboutBanner;

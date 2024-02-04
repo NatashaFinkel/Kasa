880a9e3bb4a3c9eb55ad557e4b944d3e73198902
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-02-04 16:57:59 +0100

    Création du component de la partie footer

---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+import FooterImg from "./D_Footer.png";
+import "./index.css";
+
+function Footer() {
+  return <img src={FooterImg} className="footer-display" alt="footer"></img>;
+}
+
+export default Footer;

0b03397493f0eb812a37a288f805aaa5454418c5
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-02-07 19:04:44 +0100

    Ajout d'une classe pour modifier la taille du logo Kasa en version mobile

--- src/components/header/HeaderLogo.jsx
+++ src/components/header/HeaderLogo.jsx
@@ -1,7 +1,7 @@
 import image from "../../assets/logo1.png";
 
 function HeaderLogo() {
-  return <img src={image} alt="logo"></img>;
+  return <img src={image} className="logo-kasa" alt="logo"></img>;
 }
 
 export default HeaderLogo;

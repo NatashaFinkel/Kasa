bef603732a59184af4d752827a382f1d1e9f0a32
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-02-08 08:42:25 +0100

    Ajout première version du container de la galerie

---
+++ src/components/gallery/GalleryContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./_gallery-container.scss";
+
+function GalleryContainer() {
+  <div className="gallery-container"></div>;
+}
+export default GalleryContainer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import HomeBanner from "../components/home/HomeBanner";
 
+import GalleryContainer from "../components/gallery/GalleryContainer";
+
 function Home() {
   return (
     <div>
-      <HomeBanner />
+      <div>
+        <HomeBanner />
+      </div>
+
+      <div className="gallery-container">
+        <GalleryContainer />
+      </div>
     </div>
   );
 }

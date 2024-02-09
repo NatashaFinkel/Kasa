c24f50e24fd1141573a625c8027501bb7c1a85b7
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-02-09 07:24:11 +0100

    Clean code

--- src/components/home/HomeBanner.jsx
+++ src/components/home/HomeBanner.jsx
@@ -2,9 +2,11 @@ import homeBannerImg from "../../assets/homeBanner.png";
 
 function HomeBanner() {
   return (
-    <div>
-      <img src={homeBannerImg}className="full-width" alt="bannière de la page d'accueil"></img>
-    </div>
+    <img
+      src={homeBannerImg}
+      className="full-width"
+      alt="bannière de la page d'accueil"
+    ></img>
   );
 }
 

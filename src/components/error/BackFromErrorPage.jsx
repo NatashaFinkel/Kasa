3eb5bc38d57132f337a461d1cea07d346050b43a
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-02-13 08:46:47 +0100

    Création du component de retour sur la page d'accueil à partir de la page d'erreur

---
+++ src/components/error/BackFromErrorPage.jsx
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom";
+
+function GetBackFromErrorPage() {
+  return <Link to="/">Retourner sur la page d’accueil</Link>;
+}
+
+export default GetBackFromErrorPage;

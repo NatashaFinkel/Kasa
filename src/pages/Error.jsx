42b45a5d4907967210766134fecc9936dae93803
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-02-13 08:08:39 +0100

    Mise à jour des components appelés dans la page Error

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,11 +1,17 @@
 import ErrorIMG from "../components/error/ErrorIMG";
+import ErrorStatus from "../components/error/errorStatus";
 
-function Error(){
-    return(
-        <div>
-           <ErrorIMG />
-        </div>
-    )
+function Error() {
+  return (
+    <div className="error-main-div">
+      <div>
+        <ErrorIMG />
+      </div>
+      <div>
+        <ErrorStatus />
+      </div>
+    </div>
+  );
 }
 
 export default Error;

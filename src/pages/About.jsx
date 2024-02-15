34f75429a1d726b234114c1a764f1069a4f17bac
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-02-15 12:42:37 +0100

    Ajout du component Accordion à la page about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import AboutBanner from "../components/aboutBanner/AboutBanner";
+import Accordion from "../components/accordion/Accordion";
+import "../components/accordion/_accordion.scss";
 
 function About() {
   return (
     <div>
-      <AboutBanner />
+      <div>
+        <AboutBanner />
+      </div>
+
+      <div className="about-page-main-content">
+        <Accordion />
+      </div>
     </div>
   );
 }

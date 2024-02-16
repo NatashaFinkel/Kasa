7b0be52b8a6656e06cf4a9e1a8509a3d84bd7b35
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-02-16 07:36:00 +0100

    Ajout du contenu folded-content du component Accordion et ajout du css associé

--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -9,9 +9,25 @@ function Accordion() {
         <UnfoldingArrow />
       </div>
 
+      <div className="folded-content">
+        <p className="text">
+          Les annonces postées sur Kasa garantissent une fiabilité totale. Les
+          photos sont conformes aux logements, et toutes les informations sont
+          régulièrement vérifiées par nos équipes.
+        </p>
+      </div>
+
       <div className="accordion">
         <p>Respect</p>
         <UnfoldingArrow />
+      </div>
+
+      <div className="folded-content">
+        <p className="text">
+          La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+          comportement discriminatoire ou de perturbation du voisinage
+          entraînera une exclusion de notre plateforme.
+        </p>
       </div>
 
       <div className="accordion">
@@ -19,9 +35,28 @@ function Accordion() {
         <UnfoldingArrow />
       </div>
 
+      <div className="folded-content">
+        <p className="text">
+          La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+          comportement discriminatoire ou de perturbation du voisinage
+          entraînera une exclusion de notre plateforme.
+        </p>
+      </div>
+
       <div className="accordion">
         <p>Sécurité</p>
         <UnfoldingArrow />
+      </div>
+
+      <div className="folded-content">
+        <p className="text">
+          La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
+          pour les voyageurs, chaque logement correspond aux critères de
+          sécurité établis par nos services. En laissant une note aussi bien à
+          l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les
+          standards sont bien respectés. Nous organisons également des ateliers
+          sur la sécurité domestique pour nos hôtes.
+        </p>
       </div>
     </div>
   );

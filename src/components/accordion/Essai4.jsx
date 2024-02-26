2a00e496f0a666ccfdc5eca7e99e9b9eefb1fa7a
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-02-26 16:51:58 +0100

    Suppression des anciens fichiers devenus inutiles

--- src/components/accordion/Essai4.jsx
+++
@@ -1,5 +0,0 @@
-function Essai4() {
-  console.log("Essai 4 couronné de succès !");
-}
-
-export default Essai4;
--- src/components/accordion/Essai5.jsx
+++
@@ -1,5 +0,0 @@
-function Essai5() {
-  console.log("Essai 5 couronné de succès !");
-}
-
-export default Essai5;

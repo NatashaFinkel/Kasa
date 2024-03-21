3ce145d7161a59e2617af9bc5dfde4d60ba2d81b
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-03-21 22:19:48 +0100

    Ajout des fleches droite et gauche dans le component SlideShow

--- src/components/slideShow/SlideShow.jsx
+++ src/components/slideShow/SlideShow.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { useState } from "react";
+
+import leftArrow from "../../assets/left-arrow.png";
+import rightArrow from "../../assets/right-arrow.png";
+
 import "./_slideShow.scss";
 
 const Slideshow = ({ pictures }) => {
@@ -16,10 +20,36 @@ const Slideshow = ({ pictures }) => {
     );
   }
 
-  return(
-    <div>
-         <img src={pictures[current]} alt="Le logement" className="slideShow-img"/>
+  const previousPicture = () => {
+    setCurrent(current === 0 ? allPictures - 1 : current - 1);
+  };
+
+  const nextPicture = () => {
+    setCurrent(current === allPictures - 1 ? 0 : current + 1);
+  };
+
+  return (
+    <div className="slideShow">
+      <img
+        src={pictures[current]}
+        alt="Le logement"
+        className="slideShow-img"
+      />
+      <img
+        src={rightArrow}
+        className="right-arrow"
+        onClick={nextPicture}
+        alt="flèche"
+      ></img>
+      
+      <img
+        src={leftArrow}
+        className="left-arrow"
+        onClick={previousPicture}
+        alt="flèche"
+      ></img>
+      
     </div>
-  )
+  );
 };
 export default Slideshow;

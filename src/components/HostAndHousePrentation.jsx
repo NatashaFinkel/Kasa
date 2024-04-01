76838049d1fd6ca18c034f198d1643078d4a5daf
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-04-01 10:53:15 +0200

    Utilisation du component Image dans le component HostAndHousePrentation jsx

--- src/components/HostAndHousePrentation.jsx
+++ src/components/HostAndHousePrentation.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import jsonData from "../houses.json";
+import Image from "./Image";
 import Rating from "./Rating";
 import Tags from "./Tags";
 import "../styles/_hostAndHousePresentation.scss";
@@ -23,11 +24,10 @@ function HostAndHousePresentation() {
           <div className="host-name" alt="identité du propriétaire">
             <p> {additionalDetails.host.name}</p>
           </div>
-
-          <img
+          <Image
+            src={additionalDetails.host.picture}
             className="host-picture"
             alt="Le propriétaire du logement"
-            src={additionalDetails.host.picture}
           />
         </div>
         <Rating rating={additionalDetails.rating} />

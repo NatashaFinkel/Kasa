a227335da0bf7c6dda6dbacc5158098c54d32569
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-05-08 14:18:11 +0200

    Ajout de commentaires à Collapse.jsx

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+//  <details> indique que le 
+//  contenu peut-être masqué/affiché
+//  par un clic sur son titre ( <summary> ).
+
 function Collapse({
   title,
   hiddenContent,
@@ -8,20 +12,25 @@ function Collapse({
   text,
 }) {
   return (
-    <details className={additionalDetails}>
+    < details className={additionalDetails} >
       <summary className={additionalDetailsContainer}>
         {title} <i className="fa-solid fa-chevron-up"></i>
       </summary>
-      {typeof hiddenContent === "object" ? (
-        <ul className={text}>
-          {hiddenContent.map((element) => {
-            return <li key={`${element}`}> {element}</li>;
-          })}
-        </ul>
-      ) : (
-        <p className={text}>{hiddenContent}</p>
-      )}
-    </details>
+
+      {/* On vérifie si le hiddenContent est objet.
+          Si c'est le cas, les éléments sont affichés 
+          sous forme de liste.  */}
+      {
+        typeof hiddenContent === "object" ? (
+          <ul className={text}>
+            {hiddenContent.map((element) => {
+              return <li key={`${element}`}> {element}</li>;
+            })}
+          </ul>
+        ) : (
+          <p className={text}>{hiddenContent}</p>
+        )}
+    </details >
   );
 }
 

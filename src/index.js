f55ae538bb591c9ec60e4143ce08433aa2799cee
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-05-09 07:16:48 +0200

    Modification commentaire de index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-//  Ce fichier sert de point 
-//  d'entrée principal pour 
-//  l'application KASA.
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.jsx";
+
+//  Ce fichier sert de point 
+//  d'entrée principal pour 
+//  l'application KASA.
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

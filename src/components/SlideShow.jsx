0c02b4fb50650f13a971319d645231293fa951ac
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-05-21 16:55:30 +0200

    Correction du component SlideShow

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -7,10 +7,10 @@ import "../styles/_slideShow.scss";
 
 const Slideshow = ({ pictures }) => {
 
-//  useState() sert à déclarer 
-//  la variable (ici, current) dont la
-//  valeur initiale (ici, zéro) peut être 
-//  mise à jour (ici, grâce à setCurrent() ).
+  //  useState() sert à déclarer 
+  //  la variable (ici, current) dont la
+  //  valeur initiale (ici, zéro) peut être 
+  //  mise à jour (ici, grâce à setCurrent() ).
 
   const [current, setCurrent] = useState(0);
   const allPictures = pictures.length;
@@ -25,55 +25,51 @@ const Slideshow = ({ pictures }) => {
           className="slideShow-img"
           alt="Le logement"
         />
+      </div>
+    )
+  } else if (allPictures >= 2) {
+
+    //  Le symbole "?" vérifie une condition
+    //  et retourne une valeur en fonction de cette condition.
+
+    //  Le symbole ":" sert à séparer les différentes valeurs à 
+    //  retourner selon si la condition est vraie ou fausse.
+
+    const previousPicture = () => {
+      setCurrent(current === 0 ? allPictures - 1 : current - 1);
+    };
+
+    const nextPicture = () => {
+      setCurrent(current === allPictures - 1 ? 0 : current + 1);
+    };
+
+    return (
+      <div className="slideShow">
+        <Image
+          src={pictures[current]}
+          className="slideShow-img"
+          alt="Le logement"
+        />
+
+        <Image
+          src={rightArrow}
+          className="right-arrow"
+          onClick={nextPicture}
+          alt="flèche vers la droite"
+        />
+
+        <Image
+          src={leftArrow}
+          className="left-arrow"
+          onClick={previousPicture}
+          alt="flèche vers la gauche"
+        />
 
         <div className="picture-counter">
           {current + 1}/{allPictures}
         </div>
       </div>
-    );
+    )
   }
-
-  //  Le symbole "?" vérifie une condition
-  //  et retourne une valeur en fonction de cette condition.
-
-  //  Le symbole ":" sert à séparer les différentes valeurs à 
-  //  retourner selon si la condition est vraie ou fausse.
-
-  const previousPicture = () => {
-    setCurrent(current === 0 ? allPictures - 1 : current - 1);
-  };
-
-  const nextPicture = () => {
-    setCurrent(current === allPictures - 1 ? 0 : current + 1);
-  };
-
-  return (
-    <div className="slideShow">
-      <Image
-        src={pictures[current]}
-        className="slideShow-img"
-        alt="Le logement"
-      />
-
-      <Image
-        src={rightArrow}
-        className="right-arrow"
-        onClick={nextPicture}
-        alt="flèche vers la droite"
-      />
-
-      <Image
-        src={leftArrow}
-        className="left-arrow"
-        onClick={previousPicture}
-        alt="flèche vers la gauche"
-      />
-
-      <div className="picture-counter">
-        {current + 1}/{allPictures}
-      </div>
-    </div>
-  );
-};
-
+}
 export default Slideshow;

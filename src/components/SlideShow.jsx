9e352934a928133e8511f45315742d2770afc3b6
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-05-22 17:30:09 +0200

    Correction du composant SlideShow

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -15,41 +15,9 @@ const Slideshow = ({ pictures }) => {
   const [current, setCurrent] = useState(0);
   const allPictures = pictures.length;
 
-  let pictureCounter = document.getElementsByClassName("picture-counter")[0];
-
   if (allPictures === 0) {
     return "";
   } else if (allPictures === 1) {
-pictureCounter.style.display = "none";
-   /*  return (
-      <div>
-        <Image
-          src={pictures[current]}
-          className="slideShow-img"
-          alt="Le logement"
-        />
-      </div>
-    ) */
-
-
-  } else if (allPictures >= 2) {
-
-    //  Le symbole "?" vérifie une condition
-    //  et retourne une valeur en fonction de cette condition.
-
-    //  Le symbole ":" sert à séparer les différentes valeurs à 
-    //  retourner selon si la condition est vraie ou fausse.
-
-    const previousPicture = () => {
-      setCurrent(current === 0 ? allPictures - 1 : current - 1);
-    };
-
-    const nextPicture = () => {
-      setCurrent(current === allPictures - 1 ? 0 : current + 1);
-    };
-
-    pictureCounter.style.display = "block";
-
     return (
       <div className="slideShow">
         <Image
@@ -57,26 +25,51 @@ pictureCounter.style.display = "none";
           className="slideShow-img"
           alt="Le logement"
         />
-
-        <Image
-          src={rightArrow}
-          className="right-arrow"
-          onClick={nextPicture}
-          alt="flèche vers la droite"
-        />
-
-        <Image
-          src={leftArrow}
-          className="left-arrow"
-          onClick={previousPicture}
-          alt="flèche vers la gauche"
-        />
-
-        <div className="picture-counter">
-          {current + 1}/{allPictures}
-        </div>
       </div>
-    )
+    );
   }
-}
+
+  //  Le symbole "?" vérifie une condition
+  //  et retourne une valeur en fonction de cette condition.
+
+  //  Le symbole ":" sert à séparer les différentes valeurs à 
+  //  retourner selon si la condition est vraie ou fausse.
+
+  const previousPicture = () => {
+    setCurrent(current === 0 ? allPictures - 1 : current - 1);
+  };
+
+  const nextPicture = () => {
+    setCurrent(current === allPictures - 1 ? 0 : current + 1);
+  };
+
+  return (
+    <div className="slideShow">
+      <Image
+        src={pictures[current]}
+        className="slideShow-img"
+        alt="Le logement"
+      />
+
+      <Image
+        src={rightArrow}
+        className="right-arrow"
+        onClick={nextPicture}
+        alt="flèche vers la droite"
+      />
+
+      <Image
+        src={leftArrow}
+        className="left-arrow"
+        onClick={previousPicture}
+        alt="flèche vers la gauche"
+      />
+
+      <div className="picture-counter">
+        {current + 1}/{allPictures}
+      </div>
+    </div>
+  );
+};
+
 export default Slideshow;
